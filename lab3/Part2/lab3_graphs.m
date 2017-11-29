f201189c01fc78b6168d8b80ad2787a2c7285354
Kim Larsson <kimla207@li4-16.ida.liu.se>
2017-11-29 15:51:55 +0100

    Part1 and started Part2 Part1 is done and Part2 is started, all simulations done and results extracted

---
+++ lab3/Part2/lab3_graphs.m
@@ -0,0 +1,56 @@
+clear
+close all
+format longg
+decode = dlmread('decode_result.txt');
+issue = dlmread('issue_result.txt');
+commit = dlmread('commit_result.txt');
+ruu = dlmread('ruu_result.txt');
+ialu = dlmread('ialu_result.txt');
+imult = dlmread('imult_result.txt');
+fpalu = dlmread('fpalu_result.txt');
+fpmult = dlmread('fpmult_result.txt');
+
+
+sz = 75;
+
+x = 0:10;
+
+figure
+scatter(x(1:6), decode, sz, 'r', '^', 'filled');
+hold on;
+scatter(x(1:6), issue, sz, 'g', '<', 'filled');
+scatter(x(1:6), commit, sz, 'b', '>', 'filled');
+hold off;
+axis([-1 7 0 5500000]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+ax.YAxis.TickLabelFormat = '%.0f';
+legend('decode', 'issue', 'commit');
+xlabel('Value (power of 2)')
+ylabel('Cycles')
+
+figure
+scatter(x(2:10), ruu, sz, 'r', '^', 'filled');
+axis([-1 10 0 6500000]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+ax.YAxis.TickLabelFormat = '%.0f';
+legend('ruu');
+xlabel('Value (power of 2)')
+ylabel('Cycles')
+
+
+figure
+scatter(x(1:4), ialu, sz, 'r', '^', 'filled');
+hold on;
+scatter(x(1:4), imult, sz, 'g', 'v', 'filled');
+scatter(x(1:4), fpalu, sz, 'b', '>', 'filled');
+scatter(x(1:4), fpmult, sz, 'm', '<', 'filled');
+hold off;
+axis([-1 7 0 5500000]);
+ax = gca;
+ax.YAxis.Exponent = 0;
+ax.YAxis.TickLabelFormat = '%.0f';
+legend('ialu', 'imult', 'fpalu', 'fpmult');
+xlabel('Value (power of 2)')
+ylabel('Cycles')
